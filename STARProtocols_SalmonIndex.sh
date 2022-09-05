f54504618853d425426b1b985cbce4d781a247c9
Miriam Magallón <mirimagalo@gmail.com>
2022-09-05 16:00:17 +0200

    thread parameter modified, corrected some typo mistakes

--- STARProtocols_SalmonIndex.sh
+++ STARProtocols_SalmonIndex.sh
@@ -2,15 +2,15 @@
 
 ################ Parameters #####################
 #where your index file will be stored
-index_dir= "./SalmonIndexing_Metadata" 
+index_dir="./SalmonIndexing_Metadata" 
 #Reference genome
 refGenome="./references/hg38.fa.gz"
 #Reference transcriptome
 refTranscriptome="./references/refMrna.fa.gz"
 #Salmon software path
-Salmon= "salmon"
+Salmon="salmon"
 #Threads used to run
-threads= "4"
+threads="1"
 ######### Get decoys from reference genome ########### 
 # For obtaining the decoys.txt file we followed the process described at https://combine-lab.github.io/alevin-tutorial/2019/selective-alignment/
 # Reference genome and transcriptome used for the analaysis is detailed at Mazuelas et al. 2022 
@@ -20,17 +20,16 @@ echo "*** `date` Getting Salmon index  ****"
 echo "***********************************************************"
 
 mkdir -p $index_dir
-cd $index_dir
 
 
-grep "^>" <(gunzip -c $refGenome) | cut -d " " -f 1 > decoys.txt
-sed -i.bak -e 's/>//g' decoys.txt
+grep "^>" <(gunzip -c $refGenome) | cut -d " " -f 1 > $index_dir/decoys.txt
+sed -i.bak -e 's/>//g' $index_dir/decoys.txt
 
 # concatenating transcriptome and genome reference file for index
-zcat $refTransciptome $refGenome > gentrome.fa.gz
+zcat $refTransciptome $refGenome > $index_dir/gentrome.fa.gz
 
 #Get Salmon index
-$Samlon index -t ./gentrome.fa.gz -d ./decoys.txt -p $threads -i salmon_index -k 31
+$Salmon index -t $index_dir/gentrome.fa.gz -d $index_dir/decoys.txt -p $threads -i $index_dir/salmon_index -k 31
 
 echo "*** `date` Salmon index obtained  ****"
 
